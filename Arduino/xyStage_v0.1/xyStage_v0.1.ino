6bacfdd1a81c3d00250072e29fff847cbd4190cf
Mr-Jefferson <shane.renshaw@gmail.com>
2016-10-21 13:08:42 +0800

    Arduino folder added

---
+++ Arduino/xyStage_v0.1/xyStage_v0.1.ino
@@ -0,0 +1,26 @@
+#define xstp 12 
+#define xdir 11
+#define ystp 10
+#define ydir 9
+
+const int xEnd = 
+const int xBegin = 
+const int yEnd = 
+const int yBegin = 
+
+
+
+int xEndState = 0;
+int xBeginState
+int yEndState
+int yBeginState
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
